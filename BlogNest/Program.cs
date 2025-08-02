e005413da299874baa382fa671e7799e03f13724
Viswajithm2001 <viswajithm2001@gmail.com>
2025-08-02 16:22:03 +0530

    Database is setup in context

--- BlogNest/Program.cs
+++ BlogNest/Program.cs
@@ -1,10 +1,13 @@
+using BlogNest.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-var app = builder.Build();
+builder.Services.AddDbContext<BlogDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
